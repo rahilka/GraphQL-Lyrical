ae82fa9629bfa2d80e87eb1089a8a8978bea7893
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-07-28 13:39:57 +0200

    Set new apollo client

--- client/index.js
+++ client/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';	//the Apollo Client is actually interacting with the graphQL server on the backend 
+											// is what is actually making reqests for data, and then storing that data locally when the response comes back
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+  	<ApolloProvider client={client}>
+  		<div>Lyrical</div>
+  	</ApolloProvider>
+  );
 };
 
 ReactDOM.render(
