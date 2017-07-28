418d366c92e3dde9c33aa1b370866e615ae370bf
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-07-28 12:56:27 +0200

    Added mongo lab uri

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://rahilka:123456@ds127153.mlab.com:27153/rahilkadb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
