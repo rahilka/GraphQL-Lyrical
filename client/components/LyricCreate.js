19cc846390767e6e016a88a9503cada1216cc9da
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-07-29 20:20:17 +0200

    Added lyric create component

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class LyricCreate extends Component {
+	render() {
+		return (
+
+			<form>
+				<label>Add a Lyric</label>
+				<input />
+			</form>
+
+		);
+	}
+}
+
+export default LyricCreate;
