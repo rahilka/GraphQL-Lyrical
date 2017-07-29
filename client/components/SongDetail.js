3c3a998dd73910dc01cdc2b093ffe9cf10fd5d6c
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-07-29 20:16:08 +0200

    Navigate through songs

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
+import { Link } from 'react-router';
 
 class SongDetail extends Component {
 	render() {
@@ -12,6 +13,7 @@ class SongDetail extends Component {
 
 		return(
 			<div>
+				<Link to="/">Back</Link>
 				<h3>{song.title}</h3>
 			</div>
 		);
