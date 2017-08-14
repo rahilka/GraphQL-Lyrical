cbf5a4653f80efba6676eede325430004ef45e6b
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-08-14 15:12:24 +0200

    Added optimistic response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -4,8 +4,18 @@ import gql from 'graphql-tag';
 
 class LyricList extends Component {
 
-	onLike(id) {
-		this.props.mutate({ variables: { id } })
+	onLike(id, likes) {
+		this.props.mutate({
+			variables: { id },
+			optimisticResponse: {
+				__typename: 'Mutation',
+				likeLyric: {
+					id,
+					__typename: 'LyricType',
+					likes: likes + 1
+				}
+			}
+		});
 	}
 
 	renderLyrics() {
@@ -16,7 +26,7 @@ class LyricList extends Component {
 					<div className="vote-box">
 						<i 
 							className="material-icons"
-							onClick={() => this.onLike(id)}
+							onClick={() => this.onLike(id, likes)}
 						>
 							thumb_up
 						</i>
