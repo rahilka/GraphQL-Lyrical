eac0a37aff7c04b4201f8a6c3fbb5e626d9fef12
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-08-14 14:13:36 +0200

    Added like lyric mutation

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -31,4 +31,13 @@ class LyricList extends Component {
 	}
 }
 
+const mutation = gql`
+	mutation LikeLyric($id: ID) {
+	  likeLyric(id: $id) {
+	    id
+	    likes
+	  }
+	}
+`;
+
 export default LyricList;
